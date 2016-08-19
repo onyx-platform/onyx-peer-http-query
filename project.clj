0c5be4eb5a2cfa42980116fd42617535db29a3ce
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-08-19 21:02:33 +0800

    New project to query onyx nodes

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject org.onyxplatform/onyx-node-query "0.9.9-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring "1.4.0"]
+                 [ring-jetty-component "0.3.0"]
+                 [cheshire "5.5.0"]])
---
+++ test/onyx_health/core_test.clj
@@ -0,0 +1,7 @@
+(ns onyx-health.core-test
+  (:require [clojure.test :refer :all]
+            [onyx-health.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
