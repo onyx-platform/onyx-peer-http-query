e0c0b2554b054af2816552f2ee109e4f950681c6
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-09-16 02:12:00 +0800

    Continue work on auto release

--- project.clj
+++ project.clj
@@ -9,4 +9,15 @@
                  [ring "1.4.0"]
                  [ring-jetty-component "0.3.0"]
                  [cheshire "5.5.0"]]
-  :profiles {:dev {:dependencies [[clj-http "3.2.0"]]}})
+  :repositories {"snapshots" {:url "https://clojars.org/repo"
+                              :username :env
+                              :password :env
+                              :sign-releases false}
+                 "releases" {:url "https://clojars.org/repo"
+                             :username :env
+                             :password :env
+                             :sign-releases false}}
+  :profiles {:dev {:dependencies [[clj-http "3.2.0"]]
+                   :plugins [[lein-set-version "0.4.1"]
+                             [lein-update-dependency "0.1.2"]
+                             [lein-pprint "1.1.1"]]}})
