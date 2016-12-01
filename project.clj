4476a6dd7fa7b2c97277aea7db4b5b7804df6e73
OnyxBot <michael.drogalis@onyxplatform.org>
2016-12-01 17:32:11 +0000

    Prepare for next release cycle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-peer-http-query "0.9.15.0"
+(defproject org.onyxplatform/onyx-peer-http-query "0.9.15.1-SNAPSHOT"
   :description "An Onyx health and query HTTP server"
   :url "https://github.com/onyx-platform/onyx-peer-http-query"
   :license {:name "Eclipse Public License"
