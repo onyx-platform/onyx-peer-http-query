1602f9025c3453a41045d225682d25bb61555d42
OnyxBot <michael.drogalis@onyxplatform.org>
2017-02-19 05:52:47 +0000

    Update dependency org.onyxplatform/onyx to version 0.10.0-20170219_052912-g7caddc2.
    Automatic commit by lein-unison.

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
                  [ring-jetty-component "0.3.1"]
                  [cheshire "5.7.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.10.0-20170219_053243-g0a553c2"]]
+                 [org.onyxplatform/onyx "0.10.0-20170219_052912-g7caddc2"]]
   :repositories {"snapshots" {:url "https://clojars.org/repo"
                               :username :env
                               :password :env
