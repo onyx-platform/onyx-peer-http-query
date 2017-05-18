d276d64c45e0e3c2c44dce822006f533814ca38a
OnyxBot <michael.drogalis@onyxplatform.org>
2017-05-18 02:21:19 +0000

    Release version 0.10.0.0-beta17.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.onyxplatform/onyx-peer-http-query "0.10.0.0-SNAPSHOT"
+(defproject org.onyxplatform/onyx-peer-http-query "0.10.0.0-beta17"
   :description "An Onyx health and query HTTP server"
   :url "https://github.com/onyx-platform/onyx-peer-http-query"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.10.0-beta16"]
+                 [org.onyxplatform/onyx "0.10.0-beta17"]
                  [ring/ring-core "1.5.1"]
                  [org.clojure/java.jmx "0.3.4"]
                  [ring-jetty-component "0.3.1"]
