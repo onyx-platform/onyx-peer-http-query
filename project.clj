ebf2eb5d5f5f58c822459369d12769a7ebc6bd02
OnyxBot <michael.drogalis@onyxplatform.org>
2017-09-27 06:10:13 +0000

    Update dependency org.onyxplatform/onyx to version 0.11.1-20170927_060148-g82a8527.
    Automatic commit by lein-unison.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.11.1-20170917_200609-g1438ee6"]
+                 [org.onyxplatform/onyx "0.11.1-20170927_060148-g82a8527"]
                  [ring/ring-core "1.6.2"]
                  [org.clojure/java.jmx "0.3.4"]
                  [ring-jetty-component "0.3.1"]
