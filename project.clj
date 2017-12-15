e4232c4c89804f68705c32fe1bbbc3bc8cbf393a
Lucas Bradstreet <lucasbradstreet@gmail.com>
2017-12-15 10:20:29 -0800

    Release version 0.12.0.1.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-peer-http-query "0.12.0.1-SNAPSHOT"
+(defproject org.onyxplatform/onyx-peer-http-query "0.12.0.1"
   :description "An Onyx health and query HTTP server"
   :url "https://github.com/onyx-platform/onyx-peer-http-query"
   :license {:name "Eclipse Public License"
