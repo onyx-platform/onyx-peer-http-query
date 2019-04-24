da37043cd65623222a0fd03e254943a22ef302f3
Lucas Bradstreet <lucasbradstreet@gmail.com>
2019-04-24 08:28:29 -0700

    Prepare for next release cycle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-peer-http-query "0.14.4.1"
+(defproject org.onyxplatform/onyx-peer-http-query "0.14.4.2-SNAPSHOT"
   :description "An Onyx health and query HTTP server"
   :url "https://github.com/onyx-platform/onyx-peer-http-query"
   :license {:name "Eclipse Public License"
